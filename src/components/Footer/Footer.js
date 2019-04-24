cf155afc2c0761e02633b8132f32155dba54cfe2
tearshred <vmiskeljin@gmail.com>
2019-04-23 19:59:43 -0700

    finished app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Ves Miskeljin | 2019 UCSD Coding Bootcamp</div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import michael from './images/michael.jpg';
+import dwight from './images/dwight.jpg';
+import jim from './images/jim.jpg';
+import kevin from './images/kevin.jpg';
+import meredith from './images/meredith.jpg';
+import pam from './images/pam.jpg';
+import angela from './images/angela.jpg';
+import creed from './images/creed.jpg';
+import phyllis from './images/phyllis.jpg';
+import stanley from './images/stanley.jpg';
+import oscar from './images/oscar.jpg';
+import toby from './images/toby.jpg';
+
+const images = [
+  michael,
+  dwight,
+  jim,
+  kevin,
+  meredith,
+  pam,
+  angela,
+  creed,
+  phyllis,
+  stanley,
+  oscar,
+  toby
+];
+
+export default images;
